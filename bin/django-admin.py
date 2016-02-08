3e78a43a81e899c4858c165353336c5f6e8ca60f
sandy <sandeepgr.ravi@gmail.com>
2016-02-08 12:06:38 -0800

    My django app, first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/sandy/django-user/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.9.2','django-admin.py'
+__requires__ = 'Django==1.9.2'
+__import__('pkg_resources').run_script('Django==1.9.2', 'django-admin.py')
